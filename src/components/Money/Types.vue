a5f6823c4ea5aaaff1e88b209be05f95beaebc07
Will <fangzijian1997@hotmail.com>
2020-12-25 06:43:53 +0800

    使用JS实现了一个切换组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li class="selected"> 收入</li>
+      <li :class="type==='-'&&'selected'" @click="selectType('-')">支出</li>
+      <li :class="type==='+'&&'selected'" @click="selectType('+')"> 收入</li>
     </ul>
   </div>
 </template>
 
 
 
-<script lang="ts">
+<script >
 export default {
-name: "Types"
+name: "Types",
+  //声明一个data函数用来保存当前选中的类型 +表示收入-表示支出
+  data(){
+  return {
+    type:'-'
+  }
+  },
+  methods:{
+  selectType(type){
+    if(type !=='-' && type!=='+'){
+      throw new Error ('type is unknown')
+    }
+    this.type=type
+  }
+  }
 }
 </script>
 
 
 <style lang="scss" scoped>
 .types{
-  background-color: #FFD101;
+  background-color: #d9d9d9;
   font-size: 24px;
   text-align: center;
   display: flex;
@@ -29,7 +43,7 @@ name: "Types"
     justify-content: center;
     align-items: center;
     &.selected{
-      background-color: red;
+      background-color: #FFD101;
     }
   }
 }
