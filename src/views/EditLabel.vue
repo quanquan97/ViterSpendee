7cf7587bd2180315ea3fcd364cfe7cfde6d0a63b
Will <fangzijian1997@hotmail.com>
2020-12-31 02:52:31 +0800

    展示Tag 的name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button>删除标签</Button>
@@ -27,13 +27,15 @@ import Button from '@/components/Button.vue';
   components: {FormItem,Button}
 })
 export default class EditLabel extends Vue {
+  tag?: { id: string;name: string } = undefined;
   created() {
+
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(t => t.id === id)[0];
     if (tag) {
-      console.log(tag);
+      this.tag = tag;  //通过URL获取到tag 然后把tag放到里面
     } else {
       this.$router.replace('/404');
     }
