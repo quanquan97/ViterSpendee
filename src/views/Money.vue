71969b65ef65c5e91cc8643b625dd1d74f934bf3
Will <fangzijian1997@hotmail.com>
2021-01-08 06:30:45 +0800

    所有 computed 改成 get 函数

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,58 +1,43 @@
 
 <template>
   <Layout class-prefix="layout">
-
-    <Numbers :value.sync="record.amount" @submit="saveRecord"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
-                @update:value="onUpdateNotes"/>
+                @update:value="onUpdateNotes"
+      />
     </div>
-    <Tags />
-
-
+    <Tags/>
   </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import Numbers from '@/components/Money/Numbers.vue';
+import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-
-
-
 @Component({
-  components: {FormItem, Tags, Types, Numbers},
-  computed: {
-
-    recordList() {
-      return this.$store.state.recordList;
-    }
-  }
+  components: {Tags, FormItem, Types, NumberPad},
 })
-
 export default class Money extends Vue {
-
+  get recordList() {
+    return this.$store.state.recordList;
+  }
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
-  created(){
-    this.$store.commit('fetchRecords')
+  created() {
+    this.$store.commit('fetchRecords');
   }
-
-
-
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
-
   saveRecord() {
     this.$store.commit('createRecord', this.record);
-
   }
 }
 </script>
@@ -62,11 +47,7 @@ export default class Money extends Vue {
   display: flex;
   flex-direction: column-reverse;
 }
-.notes{
+.notes {
   padding: 12px 0;
 }
-
-
 </style>
-
-
