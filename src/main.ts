868410a1bbe40726c839b3e28c10c3fd6189d08a
Will <fangzijian1997@hotmail.com>
2021-01-08 00:16:43 +0800

    使用全局 tagList

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import store from './store'
 import Nav from '@/components/Nav.vue';
 import Layout from '@/views/Layout.vue';
 import Icon from '@/components/Icon.vue';
+import tagListModel from '@/models/tagListModel';
 
 Vue.config.productionTip = false
 
@@ -13,6 +14,8 @@ Vue.config.productionTip = false
 Vue.component('Nav',Nav) //全局引入Nav  按Tab键即可直接引入
 Vue.component('Layout',Layout);
 Vue.component('Icon',Icon);
+
+window.tagList = tagListModel.fetch();
 new Vue({
   router: router,
   store,
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,79 +1,3 @@
-<!--<template>-->
-
-<!--  <Layout class-prefix="layout">-->
-<!--    {{ record }}-->
-<!--    <Numbers :value.sync="record.amount" @submit="saveRecord"/>-->
-<!--    <Types :value.sync="record.type"/>-->
-<!--    <Notes @update:value="onUpdateNotes"/>-->
-<!--    <Tags data-source.sync="tags" @update:value="onUpdateTags"/>-->
-<!--  </Layout>-->
-
-<!--</template>-->
-
-
-<!--<script lang="ts">-->
-<!--import Vue from 'vue';-->
-<!--import Numbers from '@/components/Money/Numbers.vue';-->
-<!--import Types from '@/components/Money/Types.vue';-->
-<!--import Notes from '@/components/Money/Notes.vue';-->
-<!--import Tags from '@/components/Money/Tags.vue';-->
-<!--import {Component, Watch} from 'vue-property-decorator';-->
-
-<!--// const version = window.localStorage.getItem('version')-->
-<!--const recordList: Record[]=JSON.parse(window.localStorage.getItem('recordList')||'[]');-->
-<!--//数据库升级，数据迁移-->
-<!--// if(version=='0.0.1'){recordList.forEach(record=>{record.createdAt =new Date(2020,0,1)});-->
-<!--// //保存数据-->
-<!--//   window.localStorage.setItem('recordList', JSON.stringify(recordList));-->
-<!--// }-->
-<!--// window.localStorage.setItem('version','0.0.2')-->
-
-<!--type Record = {-->
-<!--  tags: string[];-->
-<!--  notes: string;-->
-<!--  type: string;-->
-<!--  amount: number; //数据类型 object |string-->
-<!--  createdAt?: Date;//类 / 构造函数-->
-<!--}-->
-
-<!--@Component(-->
-<!--    {components: {Tags, Notes, Types, Numbers}}-->
-<!--)-->
-
-<!--export default class Money extends Vue {-->
-<!--  tags = ['衣', '食', '住', '行', '1'];-->
-
-<!--  recordList: Record[]=recordList-->
-<!--  record: Record = {tags: [], notes: '', type: '-', amount: 0};-->
-
-
-<!--  onUpdateTags(value: string[]) {this.record.tags = value; }-->
-
-<!--  onUpdateNotes(value: string) {this.record.notes = value; }-->
-
-<!--  onUpdateAmount(value: string) {this.record.amount = parseFloat(value); }-->
-
-<!--  saveRecord() {-->
-<!--    const record2: Record =JSON.parse(JSON.stringify(this.record))-->
-<!--    record2.createdAt = new Date()-->
-<!--    this.recordList.push(record2);}-->
-
-<!--  @Watch('recordList')-->
-<!--  onRecordChange() {-->
-<!--    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));-->
-<!--  }-->
-
-
-<!--}-->
-<!--</script>-->
-
-
-<!--<style lang="scss">-->
-<!--.layout-content {-->
-<!--  display: flex;-->
-<!--  flex-direction: column-reverse-->
-<!--}-->
-<!--</style>-->
 
 <template>
   <Layout class-prefix="layout">
@@ -103,19 +27,13 @@ import tagListModel from '@/models/tagListModel';
 //const model = require('@/model.js').model;//在ts文件里引入js 要使用require把它导入进来
 
 const recordList = recordListModel.fetch();
-const tagList = tagListModel.fetch();
-type RecordItem = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number; // 数据类型 object | string
-  createdAt?: Date; // 类 / 构造函数
-}
+
+
 @Component({
   components: {FormItem, Tags, Types, Numbers}
 })
 export default class Money extends Vue {
-  tags = tagList;
+  tags = window.tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
@@ -142,7 +60,6 @@ export default class Money extends Vue {
 
 <style lang="scss">
 .layout-content {
-  display: flex;
   flex-direction: column-reverse;
 }
 .notes{
