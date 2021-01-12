b0c86da0790dc26add262379423c24414ab2a72c
Will <fangzijian1997@hotmail.com>
2021-01-12 22:52:03 +0800

    自己封装 Chart 组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+    <Chart :options="x"/>
     <ol  v-if="groupedList.length>0">
       <li v-for="(group, index) in groupedList" :key="index">
         <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
@@ -26,9 +27,10 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import dayjs from 'dayjs';
-import clone from '@/lib/clone';
+import clone from '@/lib/clone'
+import Chart from '@/components/Chart.vue';
 @Component({
-  components: {Tabs},
+  components: {Tabs,Chart},
 })
 export default class Statistics extends Vue {
   tagString(tags: Tag[]) {
@@ -50,6 +52,31 @@ export default class Statistics extends Vue {
     } else {
       return day.format('YYYY年M月D日');
     }
+  }
+  get x() {
+    return {
+      xAxis: {
+        type: 'category',
+        data: [
+          '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+          '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+          '21', '22', '23', '24', '25', '26', '27', '28', '29', '30',
+        ]
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320,
+          820, 932, 901, 934, 1290, 1330, 1320, 1, 2
+        ],
+        type: 'line'
+      }],
+      tooltip: {show: true}
+    };
   }
   get recordList() {
     return (this.$store.state as RootState).recordList;
@@ -91,6 +118,11 @@ export default class Statistics extends Vue {
 </script>
 
 <style scoped lang="scss">
+.echarts {
+  max-width: 100%;
+  height: 400px;
+}
+
 .noResult {
   padding: 16px;
   text-align: center;
