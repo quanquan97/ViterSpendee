2ee0e22bcad05dc2fb9f1ab2f56a40180fdb56e3
Will <fangzijian1997@hotmail.com>
2021-01-13 16:33:14 +0800

    改echartoption 的问题

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,90 +1,85 @@
-<template>
-  <div class="tags">
-    <div class="new">
-      <button @click="createTag">新增标签</button>
-    </div>
-    <ul class="current">
-      <li v-for="tag in tagList" :key="tag.id"
-          :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)">{{tag.name}}
-      </li>
-    </ul>
-  </div>
 
-</template>
 
-<script lang="ts">
-import Vue from 'vue';
-import {Component, Prop} from 'vue-property-decorator';
-import {mixins} from 'vue-class-component';
-import TagHelper from '@/mixins/TagHelper';
-@Component
-export default class Tags extends mixins(TagHelper) {
-  selectedTags: string[] = [];
+      <template>
+        <div class="tags">
+          <div class="new">
+            <button @click="createTag">新增标签</button>
+          </div>
+          <ul class="current">
+            <li v-for="tag in tagList" :key="tag.id"
+                :class="{selected: selectedTags.indexOf(tag)>=0}"
+                @click="toggle(tag)">{{tag.name}}
+            </li>
+          </ul>
+        </div>
 
-  get tagList() {
-    return this.$store.state.tagList;
-  }
+      </template>
 
-  created() {
-    this.$store.commit('fetchTags');
-  }
-  toggle(tag: string) {
-    const index = this.selectedTags.indexOf(tag);
-    if (index >= 0) {
-      this.selectedTags.splice(index, 1);
-    } else {
-      this.selectedTags.push(tag);
-    }
-    this.$emit('update:value', this.selectedTags);
-  }
-}
-</script>
+      <script lang="ts">
 
-<style lang="scss" scoped>
-.tags {
-  display: flex;
-  background: white;
-  flex-direction: column-reverse;
-  flex-grow: 1;
-  font-size: 14px;
-  padding: 16px;
-
-  > .current {
-    display: flex;
-    flex-wrap: wrap;
-
-    > li {
-      $bg: #d9d9d9;
-      background: $bg;
-      $h: 24px;
-      height: $h;
       //想要垂直居中有两种方式：只有一行字的情况下让height等于line-height,就像此时。另一种：如果两行及其以上就用display-flex
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 18px;
-      margin-right: 12px;
-      margin-top: 4px;
 
-      & .selected {
-        background-color: darken ($bg, 50%);
-        color:white;
+      import Vue from 'vue';
+      import {Component, Prop} from 'vue-property-decorator';
+      import {mixins} from 'vue-class-component';
+      import TagHelper from '@/mixins/TagHelper';
+      @Component
+      export default class Tags extends mixins(TagHelper) {
+        selectedTags: string[] = [];
+        get tagList() {
+          return this.$store.state.tagList;
+        }
+        created() {
+          this.$store.commit('fetchTags');
+        }
+        toggle(tag: string) {
+          const index = this.selectedTags.indexOf(tag);
+          if (index >= 0) {
+            this.selectedTags.splice(index, 1);
+          } else {
+            this.selectedTags.push(tag);
+          }
+          this.$emit('update:value', this.selectedTags);
+        }
       }
+      </script>
 
-    }
-  }
-
-  > .new {
-    padding-top: 16px;
-
-    button {
-      background-color: transparent;
-      border: none;
-      border-bottom: 1px solid;
-      color: #999;
-      padding: 0 4px;
-
-    }
-  }
-}
-</style>
+      <style lang="scss" scoped>
+      .tags {
+        background: white;
+        font-size: 14px;
+        padding: 16px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
+        > .current {
+          display: flex;
+          flex-wrap: wrap;
+          > li {
+            $bg: #D9D9D9;
+            background: $bg;
+            $h: 24px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 16px;
+            margin-right: 12px;
+            margin-top: 4px;
+            &.selected {
+              background: darken($bg, 50%);
+              color: white;
+            }
+          }
+        }
+        > .new {
+          padding-top: 16px;
+          button {
+            background: transparent;
+            border: none;
+            color: #999;
+            border-bottom: 1px solid;
+            padding: 0 4px;
+          }
+        }
+      }
+      </style>
