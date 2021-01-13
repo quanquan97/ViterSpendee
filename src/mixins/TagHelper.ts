5a69adbdb715ba94a835a0ff4fb4af26d02c7b54
Will <fangzijian1997@hotmail.com>
2021-01-13 20:08:15 +0800

    修复点取消还会弹窗的bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -8,7 +8,9 @@ const map: { [key: string]: string } = {
 export class TagHelper extends Vue {
   createTag() {
     const name = window.prompt('请输入标签名');
-    if (!name) { return window.alert('标签名不能为空'); }
+    console.log(name);
+    if(name === null) return;
+    if (name.trim() === '') { return window.alert('标签名不能为空'); }
     this.$store.commit('createTag', name);
     if (this.$store.state.createTagError) {
       window.alert(map[this.$store.state.createTagError.message] || '未知错误');
